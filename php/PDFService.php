81297f2a9d417eff59096816eab99e8eefccfef1
Sergio Garcia <sergiogarcia@itop.es>
2021-04-15 20:25:04 +0100

    pruebacomposer

--- php/PDFService.php
+++ php/PDFService.php
@@ -1,18 +1,16 @@
 <?php
-/*
 include('ConvertHTML.php');
-$pdfGenerator = new PDFHTMLGenerator();
+//$pdfGenerator = new PDFHTMLGenerator();
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
-        $pdfGenerator->generarPdfString($_POST);
-        echo $pdfGenerator->getPDFBase64();
+        //$pdfGenerator->generarPdfString($_POST);
+        //echo $pdfGenerator->getPDFBase64();
+        echo "hola post";
         break;
     case 'GET':
 
         break;
 }
-*/
-echo "hola funciona";
 
 ?>
